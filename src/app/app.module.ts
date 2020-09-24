bdca7b3b2fcf3cb0914a6bcffe58505fb75271a9
devendr4 <angel.emo.altuve@gmail.com>
2020-09-23 20:52:01 -0400

    validate reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,44 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule   } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
-import { MatGridListModule } from '@angular/material/grid-list'
-import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { FormsModule } from '@angular/forms'
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { ReactiveFormsModule } from '@angular/forms'
-import {MatSelectModule} from '@angular/material/select'
-import {MatSlideToggleModule} from '@angular/material/slide-toggle'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
-
-import { MenuComponent } from './menu/menu.component'
-import { DishdetailComponent } from './dishdetail/dishdetail.component'
-import { HomeComponent } from './home/home.component'
-import { AboutComponent } from './about/about.component'
-import { ContactComponent } from './contact/contact.component'
-import { HeaderComponent } from './header/header.component'
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ContactComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -49,17 +58,11 @@ import { LoginComponent } from './login/login.component'
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-	MatSelectModule,
-	MatSlideToggleModule
+    MatSelectModule,
+    MatSlideToggleModule,
   ],
-  providers: [
-    DishService,
-    PromotionService,
-    LeaderService
-  ],
-  entryComponents:[
-    LoginComponent
-  ],
+  providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [LoginComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 @Component({
   selector: 'app-contact',
@@ -10,21 +10,38 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  @ViewChild('fform') feedbackFormDirective;
+
   constructor(private fb: FormBuilder) {
-    this.contactForm();
+    this.createForm();
   }
 
   ngOnInit(): void {}
 
   createForm() {
     this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
+      agree: false,
+      contacttype: 'None',
+      message: '',
+    });
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    alert(JSON.stringify(this.feedback));
+    this.feedbackForm.reset({
       firstname: '',
       lastname: '',
       telnum: 0,
       email: '',
       agree: false,
       contacttype: 'None',
-      message: ''
+      message: '',
     });
+    this.feedbackFormDirective.resetForm();
   }
 }
